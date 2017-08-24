b8ec675e5d9f406bfe1308f0d8c898bef88acb91
phylogram <philip.roeggla@reflex.at>
2017-08-24 14:32:23 +0200

    figure finished - from abstract to normal parent since it could be used as dev figure

---
+++ controller/game_controller/figures/ChessRule.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Philip Röggla
+ * Date: 24.08.2017
+ * Time: 12:50
+ */
+
+namespace controller\game_controller\figures;
+
+
+class ChessRule
+{
+
+}
---
+++ controller/game_controller/iterators/FigureWaveFunctionGenerator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Philip Röggla
+ * Date: 24.08.2017
+ * Time: 10:56
+ */
+
+namespace controller\game_controller\iterators;
+
+
+interface FigureWaveFunctionGenerator
+{
+
+}
